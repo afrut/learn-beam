c08ff218eebe3f68c0bc01ec889bc3c12c045e7a
eric373 <25942990+eric373@users.noreply.github.com>
2023-02-16 18:47:51 -0500

    Comment

--- basic_setup/my_package/func.py
+++ basic_setup/my_package/func.py
@@ -2,6 +2,6 @@ def cap(s: str):
     """
         Upper-case the first letter
     """
-    import pandas as pd # will fail without proper setup
+    import pandas as pd # will fail without proper setup for remote or if pandas not installed in local
     pd.Dataframe()
     return s[0].upper() + s[1:]
