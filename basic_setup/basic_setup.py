5d1618887ebddb225bcc66751498da9b1f89d0a6
eric373 <25942990+eric373@users.noreply.github.com>
2023-02-15 22:24:08 -0500

    Moved to function

--- basic_setup/basic_setup.py
+++ basic_setup/basic_setup.py
@@ -9,12 +9,7 @@ import apache_beam as beam
 import my_package.func as func
 from apache_beam.options.pipeline_options import PipelineOptions
 
-if __name__ == "__main__":
-    logging.basicConfig(level = logging.INFO)
-    beam_options = PipelineOptions()
-    opts = beam_options.get_all_options()
-    logging.info(opts["requirements_file"])
-    logging.info(opts["setup_file"])
+def run(beam_options):
     with beam.Pipeline(options = beam_options) as pipeline:
         ret = (
             pipeline
@@ -25,3 +20,12 @@ if __name__ == "__main__":
             | "Upper-case first letter" >> beam.Map(func.cap)
             | "Print" >> beam.Map(print)
         )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.INFO)
+    beam_options = PipelineOptions()
+    opts = beam_options.get_all_options()
+    logging.info(opts["requirements_file"])
+    logging.info(opts["setup_file"])
+    run(beam_options)
